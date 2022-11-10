ddd75dbb1501cbed49ed0931dffd7b86c849195c
PrecyGalaxy <adanunobid1st@gmail.com>
2022-11-10 22:15:57 +0100

    carousel project

---
+++ script.js
@@ -0,0 +1,15 @@
+let btn = document.getElementById("btn");
+
+document.addEventListener('mouseover'
+, show);
+
+document.addEventListener('mouseout'
+, disappear);
+
+function show() {
+    btn.hidden = false;
+};
+
+function show() {
+    btn.hidden = false;
+};
